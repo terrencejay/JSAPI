88abfd68cf1866f690092aa54b749f046de2786a
terrencejay <150986847+terrencejay@users.noreply.github.com>
2024-04-21 12:47:43 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+
 
 const getData = async() => {
     let response = await axios.get(`https://api.artic.edu/api/v1/artworks?ids=27992,28560&fields=id,title,image_id`)
-    console.log(response.data)
-    return response.data
+    console.log(response.data.data)
+    return response.data.data
 }
 
 //Create constants to hold DOM elements
@@ -14,14 +14,14 @@ const DOM_Elements = {
 //create the ranger list html
 const create_list = (id, name ) => {
     const html = `<a href="#" class="list-group-item list-group-item-action list-group-item-light" id="${id}">${name}</a>`
-    document.querySelector(DOM_Elements.ranger_list).insertAdjecentHTML('beforeend',html)
+    document.querySelector(DOM_Elements.ranger_list).insertAdjacentHTML('beforeend',html)
 }
 
 //Function to load data and display HTML
 const load_data = async () => {
     const rangers = await getData();
 
-    rangers.forEach( element => create_list(element.id, element.name))
+    rangers.forEach( element => create_list(element.id, element.title))
 }
 
 const clear_data = () => {
