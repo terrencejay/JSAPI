fef4286f00168ea5454729558050c1ac72117542
terrencejay <150986847+terrencejay@users.noreply.github.com>
2024-04-21 13:15:30 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-import axios from 'axios';
 
-const getData = async() => {
+async function getData() {
     let response = await axios.get(`https://api.artic.edu/api/v1/artworks?ids=27992,28560&fields=id,title,image_id`)
-    console.log(response.data)
-    return response.data
+    console.log(response.data.data)
+    return response.data.data
 }
 
-//Create constants to hold DOM elements
+
 const DOM_Elements = {
     ranger_list: '.ranger-list'
 }
 
-//create the ranger list html
-const create_list = (id, name ) => {
-    const html = `<a href="#" class="list-group-item list-group-item-action list-group-item-light" id="${id}">${name}</a>`
-    document.querySelector(DOM_Elements.ranger_list).insertAdjecentHTML('beforeend',html)
+
+const create_list = (id, name, imageId) => {
+    const html = `
+      <a href="#" class="list-group-item list-group-item-action list-group-item-light" id="${id}">
+          <img src="https://www.artic.edu/iiif/image/${imageId}/full/200,/0/default.jpg" alt="${name}" width="100" height="100">
+          ${name}
+      </a>
+  `;
+    document.querySelector(DOM_Elements.ranger_list).insertAdjacentHTML('beforeend', html);
 }
 
-//Function to load data and display HTML
-const load_data = async () => {
-    const rangers = await getData();
 
-    rangers.forEach( element => create_list(element.id, element.name))
+const load_data = async () => {
+    const rangers = await getData(); 
+
+    rangers.forEach(element => create_list(element.id, element.title, element.imageId));
 }
 
 const clear_data = () => {
